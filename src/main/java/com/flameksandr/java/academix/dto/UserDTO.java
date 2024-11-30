d87a8164ffd38f146cdd70daa0c50588df204396
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2024-11-30 16:54:15 +0300

    добавлен UserDTO

---
+++ src/main/java/com/flameksandr/java/academix/dto/UserDTO.java
@@ -0,0 +1,28 @@
+package com.flameksandr.java.academix.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    private String username;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email cannot be blank")
+    private String email;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    @NotBlank(message = "Full name cannot be blank")
+    private String fullName;
+}
+
