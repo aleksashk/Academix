2eaa61153eab371b61d0663437770db8e8e47301
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2024-11-30 16:20:17 +0300

    Добавлен Postgresql

--- src/main/java/com/flameksandr/java/academix/service/UserService.java
+++ src/main/java/com/flameksandr/java/academix/service/UserService.java
@@ -1,2 +1,35 @@
-package com.flameksandr.java.academix.service;public class UserService {
+package com.flameksandr.java.academix.service;
+
+import com.flameksandr.java.academix.exception.DuplicateEmailException;
+import com.flameksandr.java.academix.exception.DuplicateUsernameException;
+import com.flameksandr.java.academix.model.User;
+import com.flameksandr.java.academix.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(User user){
+        //Проверка на уникальность email
+        userRepository.findByEmail(user.getEmail()).ifPresent(existingUser -> {
+            throw new DuplicateEmailException("Email " + user.getEmail() + " is already taken");
+        });
+
+        //Проверка на уникальность username
+        userRepository.findByEmail(user.getUsername()).ifPresent(existingUser -> {
+            throw new DuplicateUsernameException("Username " + user.getUsername() + " is already taken");
+        });
+
+        user.setCreatedAt(LocalDateTime.now());
+        return userRepository.save(user);
+    }
+
 }
