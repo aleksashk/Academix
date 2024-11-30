445d22ec0da358470cd44d54043a66989e615e71
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2024-11-30 16:42:07 +0300

    Обновлен UserRepository

--- src/main/java/com/flameksandr/java/academix/repository/UserRepository.java
+++ src/main/java/com/flameksandr/java/academix/repository/UserRepository.java
@@ -3,9 +3,11 @@ package com.flameksandr.java.academix.repository;
 import com.flameksandr.java.academix.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByEmail(String email);
-    Optional<User> findByUsername(String username);
+
+    // Метод для проверки существования пользователя по email
+    boolean existsByEmail(String email);
+
+    // Метод для проверки существования пользователя по username
+    boolean existsByUsername(String username);
 }
