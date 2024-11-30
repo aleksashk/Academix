2ca020e98771da8353a6258cb4f83b3df59060ee
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2024-11-30 16:41:54 +0300

    Обновлен User

--- src/main/java/com/flameksandr/java/academix/model/User.java
+++ src/main/java/com/flameksandr/java/academix/model/User.java
@@ -4,14 +4,13 @@ import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
-import javax.management.relation.Role;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class User {
 
@@ -38,4 +37,14 @@ public class User {
 
     LocalDateTime updatedAt;
 
+    // Дополнительный конструктор для тестов, который исключает id
+    public User(String username, String email, String password, String fullName, Role role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.fullName = fullName;
+        this.role = role;
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
 }
