d057124853ee024346a4bb170ec61af724dd79b5
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2024-11-30 17:26:04 +0300

    удалены кастомные обработчики ошибок в связи с внедрением GlobalExceptionHandler

--- src/main/java/com/flameksandr/java/academix/exception/DuplicateUsernameException.java
+++
@@ -1,9 +0,0 @@
-package com.flameksandr.java.academix.exception;
-
-public class DuplicateUsernameException extends RuntimeException {
-    public DuplicateUsernameException(String message) {
-
-        super(message);
-
-    }
-}
