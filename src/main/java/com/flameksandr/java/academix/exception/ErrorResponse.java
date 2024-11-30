fa779cca795ae2fdefe8603235a4e91793f4b50b
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2024-11-30 19:07:31 +0300

    обновлен ErrorResponse

--- src/main/java/com/flameksandr/java/academix/exception/ErrorResponse.java
+++ src/main/java/com/flameksandr/java/academix/exception/ErrorResponse.java
@@ -1,30 +1,18 @@
 package com.flameksandr.java.academix.exception;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorResponse {
 
     private String errorCode;
     private String message;
-
-    public ErrorResponse(String errorCode, String message) {
-        this.errorCode = errorCode;
-        this.message = message;
-    }
-
-    public String getErrorCode() {
-        return errorCode;
-    }
-
-    public void setErrorCode(String errorCode) {
-        this.errorCode = errorCode;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
 
